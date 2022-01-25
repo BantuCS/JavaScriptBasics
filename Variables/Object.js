c02f582abe8e9a220ecfc5c7206e8bf023596ba0
BantuCS <63703551+BantuCS@users.noreply.github.com>
2022-01-25 18:37:34 +0530

    Add files via upload

---
+++ Variables/Object.js
@@ -0,0 +1,13 @@
+let person={
+    name:'anil',
+    age:30,
+    gender:'male',
+    location:'hyderabad'
+    };
+console.log(person);
+//to access particular attribute in an object we need to use dot operator
+console.log(person.name);
+console.log(person.age);
+//if you want to change any attribute value
+person.name='sunil';
+console.log(person);
---
+++ Variables/index.js
@@ -0,0 +1,21 @@
+let name='anil'; //String type
+let salary=50000; //Number type
+let RentedHouse=false;//Boolean type
+let LuxuryBudget=null;//Null type
+let Bonus;
+console.log(name);
+console.log(salary);
+console.log(RentedHouse);
+console.log(LuxuryBudget);
+console.log(Bonus);
+//Checking type of variables 
+console.log(typeof(name));
+console.log(typeof(salary));
+console.log(typeof(RentedHouse));
+console.log(typeof(LuxuryBudget));
+console.log(typeof(Bonus));
+//Dynamic typing
+LuxuryBudget=10000;
+console.log(typeof(LuxuryBudget));
+Bonus=true;
+console.log(typeof(Bonus));
