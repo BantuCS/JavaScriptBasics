08162da276fdb4fa31cec4758492da27917d7cd2
BantuCS <63703551+BantuCS@users.noreply.github.com>
2022-02-01 18:24:22 +0530

    Add files via upload

---
+++ Functions2/Addproject/index.js
@@ -0,0 +1,6 @@
+function add()
+{
+    let num1=parseInt(document.getElementById('num1').value);
+    let num2=parseInt(document.getElementById('num2').value);
+    console.log(num1+num2);
+}
---
+++ Functions2/Sample/index.js
@@ -0,0 +1,5 @@
+function display()
+{
+    let name=document.getElementById('uname').value;
+    console.log("Welcome "+name);
+}
